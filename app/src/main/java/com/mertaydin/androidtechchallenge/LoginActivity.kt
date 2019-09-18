cc99e398c0a6328060b2ee8e7ae59bad26818304
MERT AYDIN <mert.aydin@ug.bilkent.edu.tr>
2019-09-18 15:00:55 +0300

    Remember me implementation.

---
+++ app/src/main/java/com/mertaydin/androidtechchallenge/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.mertaydin.androidtechchallenge
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var userNameET: TextInputEditText
+    private lateinit var passwordET: TextInputEditText
+    private lateinit var rememberMeSwitch: Switch
+    private lateinit var sharedPref: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPref = getPreferences(Context.MODE_PRIVATE)
+
+        if (sharedPref.getBoolean(getString(R.string.remember_me_key), false))
+            startActivity(Intent(this, OrdersActivity::class.java))
+
+        setContentView(R.layout.activity_login)
+
+        userNameET = findViewById(R.id.user_name_et)
+        passwordET = findViewById(R.id.password_et)
+        rememberMeSwitch = findViewById(R.id.remember_me_switch)
+
+    }
+
+    fun login(view: View) {
+
+        with(sharedPref.edit()) {
+            putBoolean(getString(R.string.remember_me_key), rememberMeSwitch.isChecked)
+            commit()
+        }
+
+        val userName = userNameET.text.toString()
+        val password = passwordET.text.toString()
+
+        if (userName == "kariyer" && password == "2019ADev")
+
+            startActivity(Intent(this, OrdersActivity::class.java))
+
+    }
+}
--- app/src/main/java/com/mertaydin/androidtechchallenge/OrdersActivity.kt
+++ app/src/main/java/com/mertaydin/androidtechchallenge/OrdersActivity.kt
@@ -3,13 +3,10 @@ package com.mertaydin.androidtechchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OrdersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
+        setContentView(R.layout.activity_orders)
     }
 }
